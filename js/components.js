f83605e3fadf8f074c17ebe793b4e13aa80f856b
Joacim Nilsson <joacim@joacimnilsson.se>
2018-03-11 09:11:57 +0100

    Restructure index.html with components and change script and css src

---
+++ js/components.js
@@ -0,0 +1,56 @@
+Vue.component('purple-header', {
+  props: ['title', 'translation'],
+  template: `
+    <div class="header">
+      <div class="header-title">
+        {{title}}
+      </div>
+      <div class="header-translation">
+        {{translation}}
+      </div>
+    </div>
+  `
+});
+
+Vue.component('purple-category', {
+  props: ['icon', 'name', 'translation'],
+  template: `
+    <div class="category">
+      <div class="category-icon">
+        {{icon}}
+      </div>
+      <div class="category-name">
+        {{name}}
+      </div>
+      <div class="category-translation">
+        {{translation}}
+      </div>
+    </div>
+  `
+});
+
+Vue.component('purple-language', {
+  props: ['language', 'flag'],
+  template: `
+    <div class="language">
+      <div class="language-name">
+        {{language}}
+      </div>
+      <img class="language-flag" :src="flag">
+    </div>
+  `
+});
+
+Vue.component('purple-tutorial', {
+  props: ['background', 'logo', 'introduction'],
+  template: `
+    <div class="tutorial">
+      <img class="tutorial-background" :src="background">
+      <div class="tutorial-gradient"></div>
+      <img class="tutorial-logo" :src="logo" alt="">
+      <div class="tutorial-introduction">
+        {{introduction}}
+      </div>
+    </div>
+  `
+})
